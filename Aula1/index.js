234109eb7c48472434742acde637c300234b762c
Wolfgang Vinicius Manzato <wolfgangmanzato@outlook.com>
2025-11-20 14:57:14 -0300

    inicio das aulas de java script

---
+++ Aula1/index.js
@@ -0,0 +1,13 @@
+//Conseitos basicos 
+
+
+console.log('Nome'); // Todo o texto (string) precisa estar entre aspas, caso contrario o javascript não entende como texto.
+console.log("Nome");
+console.log(`Nome`);
+console.log("Nome 'nome'");
+console.log('Nome "nome" '); // sempre suar separadores diferentes para não confundir o javascript.
+
+console.log(123456); //numeros não precisam de aspas.
+
+console.log(12, 98.14, 'Nome');
+
