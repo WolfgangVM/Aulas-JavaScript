32722cc469a33f0f7bb1afbdee616ba61b29e99c
Wolfgang Vinicius Manzato <wolfgangmanzato@outlook.com>
2025-11-28 22:21:59 -0300

    exercicio resolvido

---
+++ Aula19/js/script.js
@@ -0,0 +1,27 @@
+function meuScopo() {
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
+
+    function recebeEventoForm (evento){
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome').value;
+        const sobrenome = form.querySelector('.sobrenome').value;
+        const peso = form.querySelector('.peso').value;
+        const altura = form.querySelector('.altura').value;
+        
+        pessoas.push({
+            nome,
+            sobrenome,
+            peso,
+            altura
+        });
+        console.log(pessoas);
+        resultado.innerHTML += `<p>${nome} ${sobrenome} ${peso} ${altura}</p>`;
+    }
+
+   form.addEventListener('submit', recebeEventoForm);
+}
+meuScopo();
